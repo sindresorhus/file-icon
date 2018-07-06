73fcb0359ae4bb9ca5fc11b6def3c4cc28e362e1
Sindre Sorhus <sindresorhus@gmail.com>
2018-07-06 15:27:02 +0700

    Code cleanups

--- Sources/file-icon/main.swift
+++ Sources/file-icon/main.swift
@@ -1,74 +1,17 @@
 import Cocoa
 
-extension NSBitmapImageRep {
-	func png() -> Data? {
-		return representation(using: .png, properties: [:])
-	}
-}
-
-extension Data {
-	var bitmap: NSBitmapImageRep? {
-		return NSBitmapImageRep(data: self)
-	}
-}
-
-extension NSImage {
-	func png() -> Data? {
-		return tiffRepresentation?.bitmap?.png()
-	}
-
-	func resizedForFile(to size: Int) -> NSImage {
-		let newSizeInt = size / Int(NSScreen.main?.backingScaleFactor ?? 1)
-		let newSize = CGSize(width: newSizeInt, height: newSizeInt)
-
-		let image = NSImage(size: newSize)
-		image.lockFocus()
-		NSGraphicsContext.current?.imageInterpolation = .high
-
-		draw(
-			in: CGRect(origin: .zero, size: newSize),
-			from: .zero,
-			operation: .copy,
-			fraction: 1
-		)
-
-		image.unlockFocus()
-		return image
-	}
-}
-
-struct CLI {
-	final class StandardErrorTextStream: TextOutputStream {
-		func write(_ string: String) {
-			FileHandle.standardError.write(string.data(using: .utf8)!)
-		}
-	}
-
-	static let stdout = FileHandle.standardOutput
-	static let stderr = FileHandle.standardError
-
-	private static var _stderr = StandardErrorTextStream()
-	static func printErr<T>(_ item: T) {
-		Swift.print(item, to: &_stderr)
-	}
-
-	static let arguments = Array(CommandLine.arguments.dropFirst(1))
-}
-
 func getIcon(input: String, size: Int) -> Data? {
-	let ws = NSWorkspace.shared
-
 	let path = (
 		input.contains(".") ?
-			ws.absolutePathForApplication(withBundleIdentifier: input) :
-			ws.fullPath(forApplication: input)
+			NSWorkspace.shared.absolutePathForApplication(withBundleIdentifier: input) :
+			NSWorkspace.shared.fullPath(forApplication: input)
 	) ?? input
 
-	if !FileManager.default.fileExists(atPath: path) {
+	guard FileManager.default.fileExists(atPath: path) else {
 		return nil
 	}
 
-	return ws.icon(forFile: path).resizedForFile(to: size).png()
+	return NSWorkspace.shared.icon(forFile: path).resizedForFile(to: size).png()
 }
 
 func getIcon(pid: Int, size: Int) -> Data? {
@@ -86,19 +29,16 @@ guard let icon: Data = {
 		return getIcon(input: input, size: size)
 	}
 }() else {
-	CLI.printErr("Couldn't find: \(input)")
+	print("Couldn't find: \(input)", to: .standardError)
 	exit(1)
 }
 
 if CLI.arguments.count >= 4 {
 	let destination = CLI.arguments[3]
 	
-	do {
+	CLI.tryOrExit {
 		try icon.write(to: URL(fileURLWithPath: destination), options: .atomic)
-	} catch {
-		CLI.printErr(error)
-		exit(1)
 	}
 }
 
-CLI.stdout.write(icon)
+CLI.standardOutput.write(icon)
---
+++ Sources/file-icon/util.swift
@@ -0,0 +1,93 @@
+import Cocoa
+
+extension NSBitmapImageRep {
+	func png() -> Data? {
+		return representation(using: .png, properties: [:])
+	}
+}
+
+extension Data {
+	var bitmap: NSBitmapImageRep? {
+		return NSBitmapImageRep(data: self)
+	}
+}
+
+extension NSImage {
+	func png() -> Data? {
+		return tiffRepresentation?.bitmap?.png()
+	}
+
+	func resizedForFile(to size: Int) -> NSImage {
+		let newSizeInt = size / Int(NSScreen.main?.backingScaleFactor ?? 1)
+		let newSize = CGSize(width: newSizeInt, height: newSizeInt)
+
+		let image = NSImage(size: newSize)
+		image.lockFocus()
+		NSGraphicsContext.current?.imageInterpolation = .high
+
+		draw(
+			in: CGRect(origin: .zero, size: newSize),
+			from: .zero,
+			operation: .copy,
+			fraction: 1
+		)
+
+		image.unlockFocus()
+		return image
+	}
+}
+
+extension FileHandle: TextOutputStream {
+	public func write(_ string: String) {
+		write(string.data(using: .utf8)!)
+	}
+}
+
+struct CLI {
+	static var standardInput = FileHandle.standardOutput
+	static var standardOutput = FileHandle.standardOutput
+	static var standardError = FileHandle.standardError
+
+	static let arguments = Array(CommandLine.arguments.dropFirst(1))
+
+	/// Execute code and print to `stderr` and exit with code 1 if it throws
+	static func tryOrExit(_ throwingFunc: () throws -> Void) {
+		do {
+			try throwingFunc()
+		} catch {
+			print(error.localizedDescription, to: .standardError)
+			exit(1)
+		}
+	}
+}
+
+enum PrintOutputTarget {
+	case standardOutput
+	case standardError
+}
+
+/// Make `print()` accept an array of items
+/// Since Swift doesn't support spreading...
+private func print<Target>(
+	_ items: [Any],
+	separator: String = " ",
+	terminator: String = "\n",
+	to output: inout Target
+) where Target: TextOutputStream {
+	let item = items.map { "\($0)" }.joined(separator: separator)
+	Swift.print(item, terminator: terminator, to: &output)
+}
+
+func print(
+	_ items: Any...,
+	separator: String = " ",
+	terminator: String = "\n",
+	to output: PrintOutputTarget = .standardOutput
+) {
+	switch output {
+	case .standardOutput:
+		print(items, separator: separator, terminator: terminator)
+	case .standardError:
+		print(items, separator: separator, terminator: terminator, to: &CLI.standardError)
+	}
+}
